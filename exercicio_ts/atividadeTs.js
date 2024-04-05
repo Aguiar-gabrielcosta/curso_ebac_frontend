10aa68e3116cfc2a2a70b066a6e87f217cd186e8
Gabriel Aguiar <Aguiar.gc@hotmail.com>
2024-04-05 14:57:56 -0300

    Atividade TS

---
+++ exercicio_ts/atividadeTs.js
@@ -0,0 +1,10 @@
+"use strict";
+function multiplicacao(valor1, valor2) {
+    return valor1 * valor2;
+}
+function saudacao(nome) {
+    return 'Olá ' + nome;
+}
+// Testes
+console.log(multiplicacao(2, 2.5));
+console.log(saudacao('Gabriel'));
---
+++ exercicio_ts/atividadeTs.ts
@@ -0,0 +1,11 @@
+function multiplicacao(valor1:number, valor2:number):number {
+    return valor1 * valor2
+}
+
+function saudacao(nome:string):string {
+    return 'Olá ' + nome 
+}
+
+// Testes
+console.log(multiplicacao(2, 2.5));
+console.log(saudacao('Gabriel'));
